5a85f0df2e2de0edcbe5bb0bc2ac38408b7c718d
prathyekareddy <prathyekaanugu@gmail.com>
2022-02-23 21:37:04 -0500

    commit

--- TestPA_HW4_SSW_567.py
+++ TestPA_HW4_SSW_567.py
@@ -7,6 +7,10 @@ class TestFetchGithubRepos(unittest.TestCase):
         with self.assertRaises(ValueError):
             fetchRepos("Triangle567")
         self.assertEqual(number_of_commits("prathyekareddy", "Triangle567"), 22)
+     def test_getHub_repo(self):
+        with self.assertRaises(ValueError):
+            fetchRepos("SSW-567")
+        self.assertEqual(number_of_commits("prathyekareddy", "SSW-567"), 4)
 
 
 if __name__ == '__main__':
