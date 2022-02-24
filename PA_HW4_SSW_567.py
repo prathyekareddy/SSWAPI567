92b061f077d9d811b3788bb5573707150fe3ba6e
prathyekareddy <prathyekaanugu@gmail.com>
2022-02-23 21:16:02 -0500

    commit

--- PA_HW4_SSW_567.py
+++ PA_HW4_SSW_567.py
@@ -1,21 +1,43 @@
-import json,requests 
+import requests
+import json
 
 
-def RepoLister(name):
-    # name = input("Enter the username: ")
-    url = "https://api.github.com/users/{}/repos".format(name)
+def fetchRepos(user_id):
+    """ Fetch user's repositories and commits"""
+    repo_storage = []
+    repo_api = "https://api.github.com/users/"
+    fetch_url = repo_api + user_id + '/repos'
 
-    r = requests.get(url)
-    z = json.loads(r.text)
-    # pprint.pprint(z)
-    for i in z:
-        url2 = "https://api.github.com/repos/{}/{}/commits".format(name, i["name"])
-        r2 = requests.get(url2)
-        z2 = json.loads(r2.text)
-        # pprint.pprint(len(z2))
-        print("Repo: " + i["name"] + " Number of commits: " + str(len(z2)))
+    get_url = requests.get(fetch_url)
+    repo_list = get_url.json()
 
-    return ("100")
+    try:
+        for line in repo_list:
+            repo = line.get('name')
+            repo_storage.append(repo)
+    except (TypeError, KeyError, IndexError, AttributeError):
+        raise ValueError('Not able to Fetch repositories from Entered User ID')
+
+    return repo_storage
+
+def number_of_commits(user_name, repo_name):
+    """ Takes input as user's id and repo name and returns number of commits in that repo """
+    get_url = requests.get('https://api.github.com/repos/{}/{}/commits'.format(user_name, repo_name))
+    num_commits = get_url.json()
+
+    if num_commits == 0:
+        print('The respective repository has no commits!')
+
+    return len(num_commits)
+
+def main():
+    """ Get user's Github ID as input """
+    user_name = input("Please enter the GitHub user ID: ")
+
+    for repo in fetchRepos(user_name):
+        num_of_commits = number_of_commits(user_name, repo)
+        print(f"Repo: {repo} Number of commits: {num_of_commits}")
 
 
-print(RepoLister("prathyekareddy"))
+if __name__ == '__main__':
+    main()
---
+++ TestPA_HW4_SSW_567.py
@@ -0,0 +1,13 @@
+import unittest
+
+from fetchGithubReposInfo import fetchRepos, number_of_commits
+
+class TestFetchGithubRepos(unittest.TestCase):
+    """ Test cases to check the results from API results"""
+    def test_getHub_repo(self):
+        with self.assertRaises(ValueError):
+            fetchRepos("Restaurant-Ratings-Project")
+        self.assertEqual(number_of_commits("anshulkapoor018", "Restaurant-Ratings-Project"), 30)
+
+
+if __name__ == '__main_
