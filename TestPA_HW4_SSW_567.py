518bfdadc7a2285e163524874eafa3455e2eb4a5
prathyekareddy <prathyekaanugu@gmail.com>
2022-03-02 17:56:46 -0500

    HW5

--- TestPA_HW4_SSW_567.py
+++ TestPA_HW4_SSW_567.py
@@ -1,11 +1,13 @@
+from unittest import mock
 import unittest
 
 from PA_HW4_SSW_567 import fetchRepos, number_of_commits
 
 class TestFetchGithubRepos(unittest.TestCase):
-    def test_getHub_repo(self):
+    @mock.patch('PA_HW4_SSW_567.number_of_commits')
+    def test_getHub_repo(self,mocked_request):
+        mocked_request.return_value = 22
         self.assertEqual(number_of_commits("prathyekareddy", "Triangle567"), 22)
-        self.assertEqual(number_of_commits("prathyekareddy", "SSW-567"), 4)
         
 if __name__ == '__main__':
     unittest.main()
