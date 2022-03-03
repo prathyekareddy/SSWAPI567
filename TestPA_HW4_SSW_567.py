61f3ed68811d0f1549440b9375d32a79940b11ad
Prathyeka Reddy Anugu <prathyekaanugu@gmail.com>
2022-03-02 20:10:21 -0500

    Mocking

--- TestPA_HW4_SSW_567.py
+++ TestPA_HW4_SSW_567.py
@@ -4,6 +4,7 @@ import unittest
 from PA_HW4_SSW_567 import fetchRepos, number_of_commits
 
 class TestFetchGithubRepos(unittest.TestCase):
+    @mock.patch('PA_HW4_SSW_567.number_of_commits')
     def test_getHub_repo(self, mocked_request):
         mocked_request.return_value = 22
         self.assertEqual(number_of_commits("prathyekareddy", "Triangle567"), 22)
